9e68ed40e463d980b1d4554d766b0acb92404145
家田真帆 <idts-3620@yahoo.ne.jp>
2020-01-23 15:07:06 +0900

    add the image in SplashView.

--- MemorizeProject/Controllers/SplashVC.swift
+++ MemorizeProject/Controllers/SplashVC.swift
@@ -10,16 +10,16 @@ import UIKit
 
 class SplashVC: UIViewController {
 
-    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.view.backgroundColor = UIColor(red: 109/255, green: 185/255, blue: 208/255, alpha: 100)
                     
-        // labelの色設定
-        self.label.textColor = .white
-        
+//        // labelの色設定
+//        self.label.textColor = .white
+//        
         // 遅延処理
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.6) {
             self.performSegue(withIdentifier: "toStart", sender: nil)
